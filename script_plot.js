cfbfbc769ea3719f063760dab25b207d918b100a
Lia Petronio <lia.marie.petronio@gmail.com>
2016-01-17 16:04:22 -0500

    circles append for prep

--- script_plot.js
+++ script_plot.js
@@ -1,19 +1,25 @@
 (function() {
 
-
+var survey_data;
 var m = {t:250,r:50,b:250,l:50},
     width = document.getElementById('plot').clientWidth - m.l - m.r,
     height = document.getElementById('plot').clientHeight - m.t - m.b;
 
-
+var scales= {};
+scales.xscore = d3.scale.ordinal().range([10, 50, 110, 150]).domain(["Minimally prepared", "Somewhat prepared", "Very prepared", "Highly prepared"]);
 
 function drawPlot1(data){
-        plot1
+        plot1.selectAll('.scores')
+        .data(data)
+        .enter()
             .append('circle')
-            .attr('cx',30)
+            .attr('cx',function(d){ return scales.xscore(d.all_prep);})
             .attr('cy',height/2)
             .attr('r',20)
             .style('fill','blue');
+
+
+console.log('data', data);
 
 }
 
@@ -39,6 +45,8 @@ function parseSurvey(d){
         lat: (+d["LocationLatitude"] == "#NULL!" ? undefined: +d["LocationLatitude"]),
         lng: (+d["LocationLongitude"] == "#NULL!" ? undefined: +d["LocationLongitude"]),
         industry: (d["INDUSTRY"] == "#NULL!" ? undefined: d["INDUSTRY"]),
+        neu_prep:(d["NU_WELL_PREP"] == "#NULL!" ? undefined: d["NU_WELL_PREP"]),
+        all_prep: (d["ALL_WELL_PREP"] == "#NULL!" ? undefined: d["ALL_WELL_PREP"]),
         // count_id: counter,
         lngLat: [+d.LocationLongitude, +d.LocationLatitude]
 
